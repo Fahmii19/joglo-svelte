2af6ec1cb12973b3996858c54a5f835e393c9e19
Fahmii19 <fhmialfi79@gmail.com>
2024-06-21 16:52:46 +0700

    chore: Update ButtonGroup component and optimize code

--- src/lib/ButtonGroup.svelte
+++ src/lib/ButtonGroup.svelte
@@ -1,50 +1,9 @@
 <script>
   import { onMount } from "svelte";
-
-  let buttons1;
-  let buttons2;
+  import { setupButtonGroups } from "../assets/js/index.js";
 
   onMount(() => {
-    buttons1 = document.querySelectorAll(".btn_chip_header1");
-    buttons2 = document.querySelectorAll(".btn_chip_header2");
-
-    buttons1.forEach((button) => {
-      button.addEventListener("click", function () {
-        if (this.classList.contains("active_btn_peta")) {
-          // Remove the 'active_btn_peta' class and add 'bg-white' if the button is already active
-          this.classList.remove("active_btn_peta");
-          this.classList.add("bg-white");
-        } else {
-          // Remove the 'active_btn_peta' class and 'bg-white' class from all buttons in group 1
-          buttons1.forEach((btn) => {
-            btn.classList.remove("active_btn_peta");
-            btn.classList.add("bg-white");
-          });
-          // Add the 'active_btn_peta' class to the clicked button and remove 'bg-white'
-          this.classList.add("active_btn_peta");
-          this.classList.remove("bg-white");
-        }
-      });
-    });
-
-    buttons2.forEach((button) => {
-      button.addEventListener("click", function () {
-        if (this.classList.contains("active_btn_peta")) {
-          // Remove the 'active_btn_peta' class and add 'bg-white' if the button is already active
-          this.classList.remove("active_btn_peta");
-          this.classList.add("bg-white");
-        } else {
-          // Remove the 'active_btn_peta' class and 'bg-white' class from all buttons in group 2
-          buttons2.forEach((btn) => {
-            btn.classList.remove("active_btn_peta");
-            btn.classList.add("bg-white");
-          });
-          // Add the 'active_btn_peta' class to the clicked button and remove 'bg-white'
-          this.classList.add("active_btn_peta");
-          this.classList.remove("bg-white");
-        }
-      });
-    });
+    setupButtonGroups();
   });
 </script>
 
