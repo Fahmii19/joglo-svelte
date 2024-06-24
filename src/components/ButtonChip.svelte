23519f4f1beacad33c53b071e1ee6748a4d55782
Fahmii19 <fhmialfi79@gmail.com>
2024-06-24 19:35:46 +0700

    chore: Update image paths for sidebar icons

--- src/components/ButtonChip.svelte
+++ src/components/ButtonChip.svelte
@@ -1,12 +1,53 @@
+<script>
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+
+  export const activeTab = writable(0);
+
+  // Button Groups
+  export function setupButtonGroups() {
+    const buttons1 = document.querySelectorAll(".btn_chip_header1");
+    const buttons2 = document.querySelectorAll(".btn_chip_header2");
+
+    function setupButtons(buttons) {
+      buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+          if (this.classList.contains("active_btn_peta")) {
+            this.classList.remove("active_btn_peta");
+            this.classList.add("bg-white");
+          } else {
+            buttons.forEach((btn) => {
+              btn.classList.remove("active_btn_peta");
+              btn.classList.add("bg-white");
+            });
+            this.classList.add("active_btn_peta");
+            this.classList.remove("bg-white");
+          }
+        });
+      });
+    }
+
+    setupButtons(buttons1);
+    setupButtons(buttons2);
+  }
+
+  // Ensure the DOM is ready before running setupButtonGroups
+  onMount(() => {
+    setupButtonGroups();
+  });
+</script>
+
 <div class="ml-3">
   <button
     class="btn_chip_header1 inline-flex justify-center items-center px-4 h-8 mx-1 text-xs font-medium rounded-full focus:outline-none focus:ring-0 bg-white"
     >&lt; Rp 1 M</button
   >
+
   <button
     class="btn_chip_header1 inline-flex justify-center items-center px-4 h-8 mx-1 text-xs font-medium rounded-full focus:outline-none focus:ring-0 bg-white"
     >Rp 1 - 3 M</button
   >
+
   <button
     class="btn_chip_header1 inline-flex justify-center items-center px-4 h-8 mx-1 text-xs font-medium rounded-full focus:outline-none focus:ring-0 bg-white"
     >&gt; Rp 3 M</button
