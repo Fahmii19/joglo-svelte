be3910d8ededbe8cc140ebc499e1269bd04a8966
Fahmii19 <fhmialfi79@gmail.com>
2024-06-27 14:11:07 +0700

    Refactor ListingAgent and StatisikAgent components

--- src/components/ListingAgent.svelte
+++ src/components/ListingAgent.svelte
@@ -1,6 +1,80 @@
 <script lang="ts">
+  import Stories from "../components/Stories.svelte";
+  import ListIcon from "../assets/images/list.png";
+  import Dropdown from "../components/Dropdown.svelte";
+  import { v4 as uuidv4 } from "uuid";
+  import Card from "../components/Card.svelte";
+  import CardNew from "../components/CardNew.svelte";
+  import { property_count, property_list } from "../store/property";
+  import type { Feature } from "../service/list_property/type";
+  import { markerStore } from "../store/map";
+
+  let property_lists: Feature[] = [];
+
+  property_list.subscribe((value) => {
+    property_lists = value;
+  });
+
+  let item_list: string[] = [
+    "Terfavorit",
+    "Populer",
+    "Random",
+    "Suprise Me !",
+    "Termahal",
+    "Termurah",
+  ];
+
+  $: property_counts = 0;
+
+  $: {
+    property_count.subscribe((value) => {
+      property_counts = value;
+    });
+  }
+
+  // Remove Marker
+  $markerStore?.remove();
 </script>
 
-<div class="h-[62.5vh] overflow-y-auto hide-scrollbar">
-  <div class="w-full h-full border border-red-500">Listing Biodata</div>
+<div class="h-full w-full flex flex-col -mt-3">
+  <div class="h-[65.2vh] story-galeri-off">
+    <div class="h-full px-0 flex flex-col">
+      <!-- Menu Button -->
+      <div
+        class="flex flex-row items-stretch justify-between mt-1 judul-dijual-kedua"
+      >
+        <div class="font-normal text-sm self-end text-gray-700 -mb-0.5">
+          Menampilkan {property_counts} data
+        </div>
+
+        <div class="">
+          <div class="relative flex flex-row">
+            <!-- Mode Card -->
+            <div class="flex items-center mr-2">
+              <div
+                class="w-9 h-9 flex items-center justify-center cursor-pointer hover:bg-slate-200 hover:rounded-lg"
+              >
+                <img
+                  class="w-7 h-7 object-contain cursor-pointer"
+                  src={ListIcon}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div
+        class="grid grid-cols-2 gap-2 pt-5 h-[59.5vh] overflow-y-auto hide-scrollbar"
+      >
+        <CardNew />
+        <CardNew />
+        <CardNew />
+        <CardNew />
+      </div>
+
+      <!--  -->
+    </div>
+  </div>
 </div>
--- src/components/StatisikAgent.svelte
+++ src/components/StatisikAgent.svelte
@@ -2,5 +2,57 @@
 </script>
 
 <div class="h-[62.5vh] overflow-y-auto hide-scrollbar">
-  <div class="w-full h-full border border-red-500">Statistik Biodata</div>
+  <!--  -->
+  <div
+    class="grid grid-row-3 pb-5 px-5 pt-4 gap-4 mode-card-kedua bg-white cursor-pointer rounded-lg shadow w-full mb-3"
+  >
+    <div class="w-full grid grid-cols-3">
+      <div class="w-full flex flex-col">
+        <div class="w-full flex ml-1 font-sf_pro_bold text-lg">7 Unit</div>
+        <div class="w-full flex text-sm ml-1">Listing Aktif</div>
+      </div>
+      <div class="w-full flex flex-col">
+        <div class="w-full flex ml-1 font-sf_pro_bold text-lg">30 Unit</div>
+        <div class="w-full flex text-sm ml-1">Terjual Bulan ini</div>
+      </div>
+      <div class="w-full flex flex-col">
+        <div class="w-full flex ml-1 font-sf_pro_bold text-lg">33 Unit</div>
+        <div class="w-full flex text-sm ml-1">Total Terjual</div>
+      </div>
+    </div>
+    <div class="w-full grid grid-cols-3">
+      <div class="w-full flex flex-col">
+        <div class="w-full flex ml-1 font-sf_pro_bold text-lg">
+          Rp 758.000.000
+        </div>
+        <div class="w-full flex text-sm ml-1">Potensi Penjualan</div>
+      </div>
+      <div class="w-full flex flex-col">
+        <div class="w-full flex ml-1 font-sf_pro_bold text-lg">
+          Rp 66.000.000.000
+        </div>
+        <div class="w-full flex text-sm ml-1">Penjualan Bulan ini</div>
+      </div>
+      <div class="w-full flex flex-col">
+        <div class="w-full flex ml-1 font-sf_pro_bold text-lg">
+          Rp 30.000.000.000
+        </div>
+        <div class="w-full flex text-sm ml-1">Total Penjualan</div>
+      </div>
+    </div>
+  </div>
+
+  <!--  -->
+
+  <div class="h-[46vh] w-full grid grid-cols-2 gap-x-3">
+    <div class="h-full bg-white rounded-lg px-3 h-[22.5vh]">
+      <canvas width="700" height="680" id="pengunjungChart"></canvas>
+    </div>
+
+    <div class="h-full bg-white rounded-lg px-3 h-[22.5vh]">
+      <canvas width="700" height="680" id="pendapatanChart"></canvas>
+    </div>
+  </div>
+
+  <!--  -->
 </div>
