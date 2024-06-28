bb71b496ac43b62929374763efde93dc5cdd5572
Fahmii19 <fhmialfi79@gmail.com>
2024-06-28 12:46:27 +0700

    feat: Add CardMiniNew component for mini card view in Gallery page

---
+++ src/components/CardMiniNew.svelte
@@ -0,0 +1,73 @@
+<div
+  class="max-w-sm bg-white cursor-pointer border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  data-active-tab="1"
+  data-object-id="1604"
+  onclick="showCardInfoDetail(this)"
+>
+  <div class="p-1 h-[5vh] bg-white my-1 rounded-t-lg select-none">
+    <div class="flex items-center justify-between w-full h-full">
+      <!-- Bagian Kiri: Gambar -->
+      <div class="flex items-center">
+        <img
+          class="w-7 h-7 rounded-full"
+          src="https://www.joglopro.com/bucket/soaraja/image/joglopro/model/model11.jpg"
+          alt="Model"
+        />
+      </div>
+
+      <!-- Bagian Tengah: Teks 'aa' -->
+      <div class="flex-grow flex items-center justify-start ml-1">
+        <div class="flex flex-col">
+          <div class="inline-flex">
+            <div
+              class="text-xs font-semibold text-black cursor-pointer"
+              onclick="showMessage(this)"
+            >
+              Budiman Teguhhh
+            </div>
+          </div>
+          <!--  -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="w-full h-[19vh] relative">
+    <div class="slider-card-mode-ketiga mode-mini-video w-full h-full">
+      <div class="relative h-[19vh] bg-black z-index-10">
+        <img
+          class="w-full h-full object-fill"
+          src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh3/rumahc1.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col p-1 w-full">
+    <!--  -->
+    <div class="w-full text-gray-500 text-xs flex justify-end">
+      <div>ID 78AC66</div>
+    </div>
+    <!--  -->
+    <div class="flex flex-row justify-between -mt-1.5">
+      <div>
+        <div class="inline-flex items-center">
+          <h5 class="text-md font-bold tracking-tight text-black">
+            Rp 11.300.000.000
+          </h5>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="flex flex-col mb-0.5 font-normal">
+      <div class="flex items-center text-xs text-black">
+        <span class="mr-1">LT 80 m² |</span>
+        <span class="mr-1">LB 276 m² |</span>
+        <span class="mr-1">2 KT |</span>
+        <span class="mr-1">5 KM</span>
+      </div>
+      <div class="text-xs text-black">Sunter, Tanjung Priok, Jakarta</div>
+    </div>
+    <!--  -->
+  </div>
+</div>
--- src/pages/Gallery.svelte
+++ src/pages/Gallery.svelte
@@ -5,6 +5,7 @@
   import { v4 as uuidv4 } from "uuid";
   import Card from "../components/Card.svelte";
   import CardNew from "../components/CardNew.svelte";
+  import CardMiniNew from "../components/CardMiniNew.svelte";
   import { property_count, property_list } from "../store/property";
   import type { Feature } from "../service/list_property/type";
   import { markerStore } from "../store/map";
@@ -34,6 +35,13 @@
 
   // Remove Marker
   $markerStore?.remove();
+
+  // State untuk mode tampilan
+  let isCardMode = true;
+
+  function toggleMode() {
+    isCardMode = !isCardMode;
+  }
 </script>
 
 <div class="h-full w-full">
@@ -56,29 +64,45 @@
             Menampilkan {property_counts} data
           </div>
 
-          <div class="">
+          <div>
             <div class="relative flex flex-row">
               <!-- Mode Card -->
               <div class="flex items-center mr-2">
-                <div
-                  class="w-9 h-9 flex items-center justify-center cursor-pointer hover:bg-slate-200 hover:rounded-lg"
+                <button
+                  class="w-9 h-9 flex items-center justify-center cursor-pointer hover:bg-slate-200 hover:rounded-lg mode-card"
+                  on:click={toggleMode}
+                  aria-label="Toggle view mode"
                 >
                   <img
                     class="w-7 h-7 object-contain cursor-pointer"
                     src={ListIcon}
                     alt=""
                   />
-                </div>
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="px-3 pt-3 grid grid-cols-2 gap-2">
+      <!-- Default Card -->
+      <div
+        class="px-3 pt-3 grid grid-cols-2 gap-2 card_default"
+        class:hidden={!isCardMode}
+      >
         {#each property_lists ?? [] as item}
           <Card previous_page="/" {item} />
         {/each}
         <CardNew />
+      </div>
+
+      <!-- MiniCard -->
+      <div
+        class="px-3 pt-3 grid grid-cols-3 gap-2 card_mini"
+        class:hidden={isCardMode}
+      >
+        <CardMiniNew />
+        <CardMiniNew />
+        <CardMiniNew />
       </div>
     </div>
   </div>
