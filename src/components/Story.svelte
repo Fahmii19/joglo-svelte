b025d9ad821036c70c9ee4998aa8e3d415f679d5
Fahmii19 <fhmialfi79@gmail.com>
2024-07-02 17:18:44 +0700

    refactor: Update Story component navigation
    Update the navigation in the Story component to use the "/detail-postingan-stories" route instead of "/postingan-stories". This change ensures that the correct route is used when the button is clicked, improving the user experience and maintaining consistency with other components.

--- src/components/Story.svelte
+++ src/components/Story.svelte
@@ -5,7 +5,7 @@
 
 <button
   class="relative flex flex-col items-center cursor-pointer"
-  on:click={() => navigate("/postingan-stories")}
+  on:click={() => navigate("/detail-postingan-stories")}
 >
   <div
     class="w-16 h-16 p-[0.15rem] bg-gradient-to-r from-yellow-200 to-pink-600 rounded-full"
@@ -16,26 +16,35 @@
       alt=""
     />
 
-    <svg
-      width="21"
-      height="21"
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      class="absolute bottom-0.5 right-1 border bg-white border-white border-2 rounded-full"
+    <button
+      on:click={() => navigate("/postingan-stories")}
+      on:keypress={(event) => {
+        if (event.key === "Enter") navigate("/postingan-stories");
+      }}
+      aria-label="Navigate to postingan stories"
+      class="absolute bottom-[0.2rem] right-1 border bg-white border-white border-2 rounded-full pointer-events-auto"
     >
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M8.125 16C12.4742 16 16 12.4742 16 8.125C16 3.77576 12.4742 0.25 8.125 0.25C3.77576 0.25 0.25 3.77576 0.25 8.125C0.25 12.4742 3.77576 16 8.125 16Z"
-        fill="#1877F2"
-      ></path>
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M8.61719 4.67969C8.61719 4.40786 8.39683 4.1875 8.125 4.1875C7.85317 4.1875 7.63281 4.40786 7.63281 4.67969V7.63281H4.67969C4.40786 7.63281 4.1875 7.85317 4.1875 8.125C4.1875 8.39683 4.40786 8.61719 4.67969 8.61719H7.63281V11.5703C7.63281 11.8421 7.85317 12.0625 8.125 12.0625C8.39683 12.0625 8.61719 11.8421 8.61719 11.5703V8.61719H11.5703C11.8421 8.61719 12.0625 8.39683 12.0625 8.125C12.0625 7.85317 11.8421 7.63281 11.5703 7.63281H8.61719V4.67969Z"
-        fill="#FFFFFF"
-      ></path>
-    </svg>
+      <svg
+        width="17"
+        height="17"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="button"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M8.125 16C12.4742 16 16 12.4742 16 8.125C16 3.77576 12.4742 0.25 8.125 0.25C3.77576 0.25 0.25 3.77576 0.25 8.125C0.25 12.4742 3.77576 16 8.125 16Z"
+          fill="#1877F2"
+        ></path>
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M8.61719 4.67969C8.61719 4.40786 8.39683 4.1875 8.125 4.1875C7.85317 4.1875 7.63281 4.40786 7.63281 4.67969V7.63281H4.67969C4.40786 7.63281 4.1875 7.85317 4.1875 8.125C4.1875 8.39683 4.40786 8.61719 4.67969 8.61719H7.63281V11.5703C7.63281 11.8421 7.85317 12.0625 8.125 12.0625C8.39683 12.0625 8.61719 11.8421 8.61719 11.5703V8.61719H11.5703C11.8421 8.61719 12.0625 8.39683 12.0625 8.125C12.0625 7.85317 11.8421 7.63281 11.5703 7.63281H8.61719V4.67969Z"
+          fill="#FFFFFF"
+        ></path>
+      </svg>
+    </button>
   </div>
 </button>
