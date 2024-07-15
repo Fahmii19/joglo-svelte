b1a932b6499495ff6e633954729fe6ab3a75364c
Fahmii19 <fhmialfi79@gmail.com>
2024-07-15 15:17:23 +0700

    refactor: Update CardKirimPesan.svelte component
    Refactor CardKirimPesan.svelte component to improve its structure, readability, and maintainability. Organize code, remove unnecessary elements, and enhance component understanding and maintainability.

---
+++ src/components/ButtonKirimPesan.svelte
@@ -0,0 +1,29 @@
+<div class="h-[6.2vh] w-full relative px-1.5">
+  <div class="w-full h-full flex items-center">
+    <input
+      type="text"
+      title=""
+      placeholder="Tuliskan pesan di sini ..."
+      class="block w-full py-2 border border-gray-300 pl-4 mx-0 text-sm text-gray-400 bg-gray-100 rounded-full outline-none focus:outline-none focus:ring-0 focus:border-gray-300 tracking-[0.030em]"
+      name="message"
+      required=""
+    />
+
+    <div class="absolute right-4 items-center inset-y-0 hidden sm:flex">
+      <input type="file" id="fileUpload" class="hidden" accept="image/*" />
+
+      <!-- Button -->
+      <button
+        type="button"
+        class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 focus:outline-none"
+        onclick="document.getElementById('fileUpload').click()"
+      >
+        <img
+          class="h-5 w-5"
+          src="https://www.joglopro.com/bucket/soaraja/image/joglopro/gallery.png"
+          alt=""
+        />
+      </button>
+    </div>
+  </div>
+</div>
---
+++ src/components/CardKirimPesan.svelte
@@ -0,0 +1,143 @@
+<div class="chat-message">
+  <div class="flex items-end justify-end">
+    <div
+      class="flex flex-col space-y-2 text-xs w-[18rem] mx-2 order-1 items-end"
+    >
+      <!--  Cards 1 -->
+      <div
+        class="w-[18rem] bg-white cursor-pointer border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="w-full h-[20vh] relative">
+          <div
+            class="slider-card-mode-pesan mode-mini-video w-full h-full slick-initialized slick-slider slick-dotted"
+          >
+            <button
+              type="button"
+              class="slick-prev slick-arrow slick-disabled"
+              aria-disabled="true"
+              style=""
+            >
+              <div class="img-wrapper-pesan">
+                <img
+                  style="margin-right: 1px;"
+                  class="custom-img-slick"
+                  src="/src/images/prev.png"
+                  alt="Previous"
+                />
+              </div>
+            </button>
+            <div class="slick-list draggable">
+              <div
+                class="slick-track"
+                style="opacity: 1; width: 574px; transform: translate3d(0px, 0px, 0px);"
+              >
+                <div
+                  class="relative h-[20vh] bg-black z-index-10 slick-slide slick-current slick-active"
+                  data-slick-index="0"
+                  aria-hidden="false"
+                  tabindex="0"
+                  role="tabpanel"
+                  id="slick-slide140"
+                  aria-describedby="slick-slide-control140"
+                  style="width: 287px;"
+                >
+                  <img
+                    class="w-full h-full object-contain custom-radis-pesan"
+                    src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh1/rumaha2.jpg"
+                    alt=""
+                  />
+                  <div class="arrow-right-custom-mini">
+                    <span class="font-semibold">NEW</span>
+                  </div>
+                </div>
+                <div
+                  class="relative h-[20vh] bg-black z-index-10 slick-slide"
+                  data-slick-index="1"
+                  aria-hidden="true"
+                  tabindex="-1"
+                  role="tabpanel"
+                  id="slick-slide141"
+                  aria-describedby="slick-slide-control141"
+                  style="width: 287px;"
+                >
+                  <img
+                    class="w-full h-full object-fill"
+                    src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh1/rumaha2.jpg"
+                    alt=""
+                  />
+                  <div class="arrow-right-custom-mini">
+                    <span class="font-semibold">NEW</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <button
+              type="button"
+              class="slick-next slick-arrow"
+              style=""
+              aria-disabled="false"
+            >
+              <div class="img-wrapper-pesan">
+                <img
+                  style="margin-left: 1px;"
+                  class="custom-img-slick"
+                  src="/src/images/next.png"
+                  alt="Next"
+                />
+              </div>
+            </button>
+            <ul class="slick-dots" style="" role="tablist">
+              <li class="slick-active" role="presentation">
+                <button
+                  type="button"
+                  role="tab"
+                  id="slick-slide-control140"
+                  aria-controls="slick-slide140"
+                  aria-label="1 of 2"
+                  tabindex="0"
+                  aria-selected="true">1</button
+                >
+              </li>
+              <li role="presentation">
+                <button
+                  type="button"
+                  role="tab"
+                  id="slick-slide-control141"
+                  aria-controls="slick-slide141"
+                  aria-label="2 of 2"
+                  tabindex="-1">2</button
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="flex flex-col p-1 ml-1 w-full h-[9vh]">
+          <!--  -->
+          <div class="flex flex-row justify-between mt-1">
+            <div>
+              <div class="inline-flex items-center">
+                <h5
+                  class="text-md font-bold tracking-tight text-black"
+                  style="font-size: 1rem"
+                >
+                  Rp 11.300.000.000
+                </h5>
+              </div>
+            </div>
+          </div>
+          <!--  -->
+          <div class="flex flex-col mb-0 mt-1 font-normal">
+            <div class="flex items-center text-[11px] text-black">
+              <span class="mr-2">LT 80 m² |</span>
+              <span class="mr-2">LB 276 m² |</span>
+              <span class="mr-2">2 KT |</span>
+              <span class="mr-2">5 KM</span>
+            </div>
+            <div class="text-xs text-black">Sunter, Tanjung Priok, Jakarta</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
