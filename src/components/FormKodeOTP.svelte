4a12f21b7996b0dfef8c2381748def6d1e4b86fe
Fahmii19 <fhmialfi79@gmail.com>
2024-07-17 17:31:43 +0700

    refactor: Update FormKodeOTP.svelte component to improve OTP form layout and functionality

--- src/components/FormKodeOTP.svelte
+++ src/components/FormKodeOTP.svelte
@@ -7,45 +7,65 @@
     // Logic untuk reset kata sandi, misalnya mengarahkan ke halaman login atau menampilkan pesan sukses
     navigate("/login");
   }
+
+  function Batal() {
+    // Logic untuk batal, misalnya mengarahkan ke halaman login
+    navigate("/login");
+  }
 </script>
 
 <div
-  class="h-[45vh] w-[25vw] flex justify-center items-center p-5 rounded-xl bg-white rounded-lg"
+  class="h-[37vh] w-[25vw] flex flex-col justify-between p-5 rounded-xl bg-white"
 >
   <div class="w-full">
-    <p class="text-center text-sm text-black mt-10">
+    <p class="text-center text-sm text-black">
       Harap periksa pesan teks berisi kode di ponsel Anda. Kode Anda memiliki
       panjang 6 karakter.
     </p>
+  </div>
 
-    <form action="" class="my-3">
+  <div class="w-full">
+    <form action="" class="">
       <div class="flex flex-col">
         <div class="flex flex-col space-y-3 mb-3 mt-5">
           <div>
             <div class="mb-3 pt-0">
               <input
-                type="text"
+                type="number"
                 placeholder="Masukan OTP"
                 class="px-3 py-4 border text-black bg-white rounded-lg text-sm focus:outline-none focus:ring-0 w-full remove_style_arrow_inputan"
               />
             </div>
           </div>
         </div>
-
-        <button
-          type="button"
-          on:click={MasukAkunLogin}
-          class="w-full py-2 mt-3 mb-5 font-medium text-white text-sm bg-[#0394F7] hover:bg-[#1877F2] rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
-        >
-          <span>Kirim</span>
-        </button>
       </div>
     </form>
   </div>
+  <!--  -->
+
+  <div class="flex space-x-3">
+    <button
+      type="button"
+      on:click={MasukAkunLogin}
+      class="w-full py-2 font-medium text-white text-sm bg-[#0394F7] hover:bg-[#1877F2] rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
+    >
+      <span>Kirim</span>
+    </button>
+
+    <button
+      type="button"
+      on:click={Batal}
+      class="w-full py-2 font-medium text-white text-sm bg-gray-400 hover:bg-gray-500 rounded-lg border-gray-500 hover:shadow inline-flex space-x-2 items-center justify-center"
+    >
+      <span>Batal</span>
+    </button>
+  </div>
+
+  <!--  -->
 </div>
 
 <style>
   input:focus {
-    border-color: gray; /* Mengubah warna border menjadi abu-abu saat difokuskan */
+    border-color: gray;
   }
 </style>
