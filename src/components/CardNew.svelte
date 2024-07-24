63976580abf641f8f3f3cb09a9d3919b598d522c
Fahmii19 <fhmialfi79@gmail.com>
2024-07-24 14:10:47 +0700

    refactor: Add carousel functionality to CardNew.svelte component
    Introduce a carousel component to the CardNew.svelte component, allowing users to navigate through multiple images. This enhances the visual presentation of the component and provides a better user experience.

--- src/components/CardNew.svelte
+++ src/components/CardNew.svelte
@@ -13,6 +13,33 @@
   const handleEditListingClick = () => {
     buttonEditListing.set(true);
   };
+
+  import Carousel from "svelte-carousel";
+  let carousel: any;
+  let currentIndex = 0;
+
+  import NextIcon from "../assets/images/next.png";
+  import PrevIcon from "../assets/images/prev.png";
+
+  const goToPrevPage = (event) => {
+    event.stopPropagation(); // Menghentikan propagasi event
+    if (carousel) {
+      carousel.goToPrev();
+      currentIndex = Math.max(currentIndex - 1, 0);
+    }
+  };
+
+  const goToNextPage = (event) => {
+    event.stopPropagation(); // Menghentikan propagasi event
+    if (carousel) {
+      carousel.goToNext();
+      currentIndex = Math.min(currentIndex + 1, 2); // Asumsi ada 3 gambar (index 0, 1, 2)
+    }
+  };
+
+  const handleAfterChange = (event) => {
+    currentIndex = event.detail.currentSlide;
+  };
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -22,7 +49,6 @@
   id="card-property-1"
   on:click={() => navigate("/detail-new")}
 >
-  <!--  -->
   <div class="py-1 px-2 h-[7vh]">
     <div class="w-full h-full grid grid-cols-[1fr,40px]">
       <div class="w-full grid grid-cols-[33px,1fr] items-center">
@@ -36,11 +62,10 @@
         <div class="w-full flex flex-col ml-2">
           <div class="h-full flex items-center inline-flex mt-0.5">
             <div class="text-sm font-semibold text-black cursor-pointer">
-              <div class="tracking-[0.060em]">Gunawan Budi Ardivaa2</div>
+              <div class="tracking-[0.060em]">Gunawan Budi Ardivaa23</div>
             </div>
             <div class="flex items-center justify-center ml-1"></div>
           </div>
-          <!--  -->
         </div>
       </div>
       {#if isVisible}
@@ -62,25 +87,89 @@
       {/if}
     </div>
   </div>
-  <!--  -->
   <div class="w-full h-[27vh] relative">
-    <img class="w-full h-full object-fill" src={RumahContoh1} alt="" />
-  </div>
+    <div class="grid grid-cols-1 items-center justify-center">
+      <div class="w-full col-span-full relative">
+        <Carousel
+          bind:this={carousel}
+          particlesToShow={1}
+          particlesToScroll={1}
+          infinite={false}
+          dots={false}
+          arrows={false}
+          on:afterChange={handleAfterChange}
+        >
+          <!-- Gambar di Carousel -->
+          <div>
+            <img
+              class="w-full h-full object-fill"
+              src={RumahContoh1}
+              alt="Rumah Contoh 1"
+            />
+          </div>
+          <div>
+            <img
+              class="w-full h-full object-fill"
+              src={RumahContoh1}
+              alt="Rumah Contoh 2"
+            />
+          </div>
+          <div>
+            <img
+              class="w-full h-full object-fill"
+              src={RumahContoh1}
+              alt="Rumah Contoh 3"
+            />
+          </div>
+        </Carousel>
 
-  <!--  -->
-  <div class="px-2 pt-[0.3rem] h-[11.3vh]">
+        <!-- Tombol Sebelumnya -->
+        {#if currentIndex > 0}
+          <button
+            on:click={goToPrevPage}
+            class="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 ml-3"
+          >
+            <div class="img-wrapper">
+              <img
+                style="margin-right: 1px;"
+                class="custom-img-slick"
+                src={PrevIcon}
+                alt="Previous"
+              />
+            </div>
+          </button>
+        {/if}
+
+        <!-- Tombol Berikutnya -->
+        {#if currentIndex < 2}
+          <button
+            on:click={goToNextPage}
+            class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 mr-3"
+          >
+            <div class="img-wrapper">
+              <img
+                style="margin-left: 1px;"
+                class="custom-img-slick"
+                src={NextIcon}
+                alt="Next"
+              />
+            </div>
+          </button>
+        {/if}
+      </div>
+    </div>
+  </div>
+  <div class="px-2 pt-[0.4rem] h-[11.3vh]">
     <div class="w-full text-gray-500 text-xs flex justify-end">
       <div>ID 78AC66</div>
     </div>
     <div class="flex flex-row justify-between -mt-1">
       <div>
-        <!-- svelte-ignore a11y-invalid-attribute -->
         <div>
           <div class="inline-flex items-center w-full">
             <h5 class="text-lg font-bold tracking-tight text-black">
               Rp 11.300.000.000
             </h5>
-
             <div class="flex flex-row items-center justify-center"></div>
           </div>
         </div>
@@ -98,8 +187,6 @@
           Sunter, Tanjung Priok, Jakarta Utara
         </div>
       </div>
-      <!--  -->
     </div>
   </div>
-  <!--  -->
 </div>
