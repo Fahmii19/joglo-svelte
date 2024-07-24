628080c465e41022252bbd8f8a145420fb4004dd
Fahmii19 <fhmialfi79@gmail.com>
2024-07-24 14:41:36 +0700

    refactor: Add carousel functionality to CardMiniNew.svelte component

--- src/components/CardMiniNew.svelte
+++ src/components/CardMiniNew.svelte
@@ -1,5 +1,33 @@
-<script>
+<script lang="ts">
   import { navigate } from "svelte-routing";
+  import Carousel from "svelte-carousel";
+
+  let carousel: any;
+  let currentIndex = 0;
+
+  import NextIcon from "../assets/images/next.png";
+  import PrevIcon from "../assets/images/prev.png";
+  import Sold from "../assets/images/sold2.png";
+
+  const goToPrevPage = (event) => {
+    event.stopPropagation(); // Menghentikan propagasi event
+    if (carousel) {
+      carousel.goToPrev();
+      currentIndex = Math.max(currentIndex - 1, 0);
+    }
+  };
+
+  const goToNextPage = (event) => {
+    event.stopPropagation(); // Menghentikan propagasi event
+    if (carousel) {
+      carousel.goToNext();
+      currentIndex = Math.min(currentIndex + 1, 2); // Asumsi ada 3 gambar (index 0, 1, 2)
+    }
+  };
+
+  const handleAfterChange = (event) => {
+    currentIndex = event.detail.currentSlide;
+  };
 
   function showMessage(element) {
     alert(`You clicked on ${element.innerText}`);
@@ -28,7 +56,7 @@
         />
       </div>
 
-      <!-- Bagian Tengah: Teks 'Budiman Teguhhh' -->
+      <!-- Bagian Tengah: Teks 'Budiman Teguh' -->
       <div class="flex-grow flex items-center justify-start ml-1">
         <div class="flex flex-col">
           <div class="inline-flex">
@@ -36,10 +64,10 @@
               class="text-xs font-semibold text-black cursor-pointer"
               role="button"
               tabindex="0"
-              on:click={() => showMessage(event.target)}
+              on:click={(event) => showMessage(event.target)}
               on:keydown={(e) => {
                 if (e.key === "Enter") {
-                  showMessage(event.target);
+                  showMessage(e.target);
                 }
               }}
             >
@@ -54,11 +82,73 @@
   <div class="w-full h-[19vh] relative">
     <div class="slider-card-mode-ketiga mode-mini-video w-full h-full">
       <div class="relative h-[19vh] bg-black z-index-10">
-        <img
-          class="w-full h-full object-fill"
-          src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh3/rumahc1.jpg"
-          alt=""
-        />
+        <Carousel
+          bind:this={carousel}
+          particlesToShow={1}
+          particlesToScroll={1}
+          infinite={false}
+          dots={false}
+          arrows={false}
+          on:afterChange={handleAfterChange}
+        >
+          <div class="w-full h-full relative">
+            <img
+              class="w-full h-full rounded-lg object-contain"
+              src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh1/rumaha1.jpg"
+              alt="Gambar Pertama"
+            />
+          </div>
+
+          <div class="w-full h-full relative">
+            <img
+              class="w-full h-full rounded-lg object-contain"
+              src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh1/rumaha1.jpg"
+              alt="Gambar Kedua"
+            />
+          </div>
+
+          <div class="w-full h-full relative">
+            <img
+              class="w-full h-full rounded-lg object-contain"
+              src="https://www.joglopro.com/bucket/soaraja/image/joglopro/Hunian/Rmh1/rumaha1.jpg"
+              alt="Gambar Ketiga"
+            />
+          </div>
+        </Carousel>
+
+        <!-- Tombol Sebelumnya -->
+        {#if currentIndex > 0}
+          <button
+            on:click={goToPrevPage}
+            class="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 ml-3"
+          >
+            <div class="img-wrapper">
+              <img
+                style="margin-right: 1px;"
+                class="custom-img-slick"
+                src={PrevIcon}
+                alt="Previous"
+              />
+            </div>
+          </button>
+        {/if}
+
+        <!-- Tombol Berikutnya -->
+        {#if currentIndex < 2}
+          <button
+            on:click={goToNextPage}
+            class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 mr-3"
+          >
+            <div class="img-wrapper">
+              <img
+                style="margin-left: 1px;"
+                class="custom-img-slick"
+                src={NextIcon}
+                alt="Next"
+              />
+            </div>
+          </button>
+        {/if}
       </div>
     </div>
   </div>
