72e93cfae125d461f1539c4dcf0e792e68f8565c
Fahmii19 <fhmialfi79@gmail.com>
2024-07-24 14:58:06 +0700

    refactor: Improve carousel functionality and layout in Stories.svelte component

--- src/components/Stories.svelte
+++ src/components/Stories.svelte
@@ -4,38 +4,44 @@
   import StoryDilihat from "./StoryDilihat.svelte";
   import StoryBelumDilihat from "./StoryBelumDilihat.svelte";
   import StoryTidakAda from "./StoryTidakAda.svelte";
+
   let carousel: any;
+  let currentIndex = 0;
+  let totalItems = 18; // Jumlah total story yang ada
 
   import NextIcon from "../assets/images/next.png";
   import PrevIcon from "../assets/images/prev.png";
 
   const goToPrevPage = () => {
-    carousel.goToPrev();
+    if (carousel) {
+      carousel.goToPrev();
+      currentIndex = Math.max(currentIndex - 5, 0);
+    }
   };
 
   const goToNextPage = () => {
-    carousel.goToNext();
+    if (carousel) {
+      carousel.goToNext();
+      currentIndex = Math.min(currentIndex + 5, totalItems - 9);
+    }
+  };
+
+  const handleAfterChange = (event) => {
+    currentIndex = event.detail.currentSlide;
   };
 </script>
 
 <div class="px-2 py-2">
-  <!--  -->
   <div class="flex justify-center">
-    <div class="w-[47vw]">
+    <div class="w-[44vw]">
       <div class="grid grid-cols-12 items-center justify-center">
-        <div class="w-full col-span-full">
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <Carousel
-            bind:this={carousel}
-            particlesToShow={9}
-            particlesToScroll={5}
-            infinite={false}
-            dots={false}
-          >
-            <!-- svelte-ignore missing-declaration -->
-            <!-- svelte-ignore missing-declaration -->
-            <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <button slot="prev" on:click={goToPrevPage}>
+        <div class="w-full col-span-full relative">
+          <!-- Tombol Sebelumnya -->
+          {#if currentIndex > 0}
+            <button
+              on:click={goToPrevPage}
+              class="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 -ml-7"
+            >
               <div class="img-wrapper">
                 <img
                   style="margin-right: 1px;"
@@ -45,13 +51,21 @@
                 />
               </div>
             </button>
-            <!--  -->
+          {/if}
+
+          <!-- Carousel -->
+          <Carousel
+            bind:this={carousel}
+            particlesToShow={9}
+            particlesToScroll={5}
+            infinite={false}
+            dots={false}
+            arrows={false}
+            on:afterChange={handleAfterChange}
+          >
             <Story self_story={true} />
-            <!--  -->
             <StoryDilihat />
-            <!--  -->
             <StoryTidakAda />
-            <!--  -->
             <StoryBelumDilihat />
             <StoryBelumDilihat />
             <StoryBelumDilihat />
@@ -67,10 +81,14 @@
             <StoryBelumDilihat />
             <StoryBelumDilihat />
             <StoryBelumDilihat />
-            <StoryBelumDilihat />
-            <!-- svelte-ignore missing-declaration -->
-            <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <button slot="next" on:click={goToNextPage}>
+          </Carousel>
+
+          <!-- Tombol Berikutnya -->
+          {#if currentIndex < totalItems - 9}
+            <button
+              on:click={goToNextPage}
+              class="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 -mr-7"
+            >
               <div class="img-wrapper">
                 <img
                   style="margin-left: 1px;"
@@ -80,7 +98,7 @@
                 />
               </div>
             </button>
-          </Carousel>
+          {/if}
         </div>
       </div>
     </div>
